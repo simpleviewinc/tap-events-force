1910118d5cdbb02fb83e49e76cdbee241cb7be89
lancetipton <lancetipton04@gmail.com>
2020-02-05 18:33:33 -0700

    Updates   * Minor changes to tap   * Removed assets path   * Updated cp for doing local dev work

--- cp.sh
+++ cp.sh
@@ -1,3 +1,25 @@
-# for development with sv-keg until we get it set up as a proper node module
-#rm -rf node_modules/sv-keg
-#cp -rf ~/zerista/repos/sv-keg-expo node_modules/sv-keg
+# When developing on the sv-keg
+# rm -rf node_modules/sv-keg/App.js
+# rm -rf node_modules/sv-keg/app.json
+# rm -rf node_modules/sv-keg/babel.config.js
+# rm -rf node_modules/sv-keg/core
+# cp -Rf ~/zerista/repos/sv-keg/App.js node_modules/sv-keg/App.js
+# cp -Rf ~/zerista/repos/sv-keg/app.json node_modules/sv-keg/app.json
+# cp -Rf ~/zerista/repos/sv-keg/babel.config.js node_modules/sv-keg/babel.config.js
+# cp -Rf ~/zerista/repos/sv-keg/core node_modules/sv-keg/core
+
+# When developing on the sv-tap-resolver
+# rm -rf node_modules/sv-keg/node_modules/tap-resolver/src
+# rm -rf node_modules/sv-keg/node_modules/tap-resolver/babel.config.js
+# cp -Rf ~/zerista/repos/sv-tap-resolver/src node_modules/sv-keg/node_modules/tap-resolver/src
+# cp -Rf ~/zerista/repos/sv-tap-resolver/babel.config.js node_modules/sv-keg/node_modules/tap-resolver/babel.config.js
+
+# When developing on the re-theme
+# rm -rf ~/zerista/repos/sv-keg/node_modules/re-theme/build
+# cp -Rf ~/zerista/repos/sv-re-theme/build ~/zerista/repos/sv-keg/node_modules/re-theme/build
+
+# When developing on the jsutils
+# rm -rf ~/zerista/repos/sv-keg/node_modules/jsutils/cjs
+# rm -rf ~/zerista/repos/sv-keg/node_modules/jsutils/esm
+# cp -Rf ~/zerista/repos/jsUtils/cjs ~/zerista/repos/sv-keg/node_modules/jsutils/cjs
+# cp -Rf ~/zerista/repos/jsUtils/esm ~/zerista/repos/sv-keg/node_modules/jsutils/esm
