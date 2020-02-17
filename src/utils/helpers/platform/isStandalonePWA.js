4663897c86440895a0df007c4922268d14eba3f9
Michael Carolin <michael.carolin@simpleviewinc.com>
2020-02-17 14:44:17 -0700

    improved window/navigator checking for RN

--- src/utils/helpers/platform/isStandalonePWA.js
+++ src/utils/helpers/platform/isStandalonePWA.js
@@ -1,9 +1,11 @@
+const hasWindow = typeof window !== 'undefined'
+const hasNavigator = typeof navigator !== 'undefined'
 /**
  * @returns { Boolean } - true if the app is running as a standalone pwa on web
  */
 export const isStandalonePWA = () => {
   // if window or navigator are undefined, then we are not on web (probably native)
-  if (!window || !navigator) return false
+  if (!hasWindow || !hasNavigator) return false
 
   const query = '(display-mode: standalone)'
   return navigator.standalone || window.matchMedia(query).matches
