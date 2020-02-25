f90b2e1f2eb1dab7f0e094b70afc8d8f033cf295
Daniel Yo <dnl992004@yahoo.com.au>
2020-02-25 14:06:38 -0700

    update tapIndex theme

--- src/theme/tapIndex.js
+++ src/theme/tapIndex.js
@@ -1,7 +1,10 @@
+import { deepMerge } from 'jsutils'
 import { app } from './app'
 import { modal } from './modal'
 import { shadow } from './shadow'
+import { theme as kegComponentsTheme } from 'keg-components'
 
-export const theme = {
+export const theme = deepMerge(
+  kegComponentsTheme,
   app, modal, shadow
-} 
+)
