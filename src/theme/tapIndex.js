28bbc9fbf9a244a2abfedfc8552f131915c4f0d4
Daniel Yo <dyo@simpleviewinc.com>
2020-03-04 08:44:25 -0700

    keg-components reference update (#19)
    * update:
    - removed keg-components from dependencies
    - updated appHeader prop
    - imported keg-components and its theme from keg-core
    
    * update package json

--- src/theme/tapIndex.js
+++ src/theme/tapIndex.js
@@ -2,7 +2,7 @@ import { deepMerge } from 'jsutils'
 import { app } from './app'
 import { modal } from './modal'
 import { shadow } from './shadow'
-import { theme as kegComponentsTheme } from 'keg-components'
+import { kegComponentsTheme } from 'SVTheme/kegComponentsTheme'
 
 export const theme = deepMerge(
   kegComponentsTheme,
