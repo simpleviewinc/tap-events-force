8a54bab5f524272652967f54c4168647e2d352ed
Daniel Yo <dnl992004@yahoo.com.au>
2020-06-15 15:10:12 -0700

    stash

--- src/containers/sessionsContainer.js
+++ src/containers/sessionsContainer.js
@@ -1,5 +1,9 @@
-
+import React from 'react'
+import { Text } from 'SVComponents'
 
 export const SessionsContainer = (props) => {
 
+  return (
+  <Text>{'Session Container placeholder'}</Text>
+  )
 }
