66b404e9ea5d0b34377294f793c10fd414b10533
Daniel Yo <dyo@simpleviewinc.com>
2020-06-22 10:47:51 -0700

    Zen 296 sessions container (#24)
    * update:
    - updated gitignore
    - added sessionsContainer
    - updated rootContainer to load sessionsContainer
    - moved the old contents of rootContainer to testContainer
    
    * stash
    
    * update:
    - update package json
    
    * added session container btn in root to navigate
    
    * format update
    
    * added pr template
    
    * added app header to sessions container
    
    * minor updates

--- src/containers/rootContainer.js
+++ src/containers/rootContainer.js
@@ -1,50 +1,8 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-import { useTheme } from '@simpleviewinc/re-theme'
-import { Values } from 'SVConstants'
-import { get } from 'jsutils'
-import { useCollection } from 'SVHooks/useCollection'
-import { Button, withAppHeader } from 'SVComponents'
-import { navigateTo } from 'SVActions/navigation/navigateTo'
-import { isStandalonePWA } from 'SVUtils/platform'
+import { withAppHeader } from 'SVComponents'
+import { SessionsContainer } from 'SVContainers'
 import { displayName } from 'SVConfig'
 
-const { events, sessions } = Values.categories
-
 export const RootContainer = withAppHeader(displayName, props => {
-  const theme = useTheme()
-
-  // loads and starts watching the collections.
-  // Placing this here until we start working on the screens that will have their own containers to call these functions.
-  const collections = {
-    [events]: useCollection(events),
-    [sessions]: useCollection(sessions),
-  }
-
-  return (
-    <View
-      style={theme.join(
-        get(theme, [ 'app', 'container' ]),
-        get(props, [ 'styles', 'container' ])
-      )}
-    >
-      { isStandalonePWA() ? <Text>PWA</Text> : <Text>Not PWA</Text> }
-
-      { [ events, sessions ].map(coll => (
-        <React.Fragment key={coll}>
-          <Text>{ `Number of ${coll}:` }</Text>
-
-          <Text>{ Object.keys(collections[coll]).length } </Text>
-        </React.Fragment>
-      )) }
-      <Button onPress={() => navigateTo('/test')}>Navigate (test)</Button>
-
-      <Button
-        style={{ marginTop: 15 }}
-        onPress={() => navigateTo('/qr')}
-      >
-        Scan QR Code
-      </Button>
-    </View>
-  )
+  return <SessionsContainer />
 })
---
+++ src/containers/sessionsContainer.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Text } from 'SVComponents'
+
+export const SessionsContainer = props => {
+  return <Text>Session Container placeholder</Text>
+}
