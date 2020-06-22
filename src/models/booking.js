4db4472fdd358e39ba7941711980322d7f050b8e
Daniel Yo <dnl992004@yahoo.com.au>
2020-06-22 10:55:47 -0700

    minor update

--- src/models/booking.js
+++ src/models/booking.js
@@ -1,15 +1,14 @@
 import { deepFreeze } from 'jsutils'
-import { Values } from 'SVConstants'
 
 /**
  * Session booking item
  * @typedef booking
  * @type {object}
- * @property {('PERSON'|'GROUP'|'UNDEFINED')} type
+ * @property {('PERSON'|'GROUP')} type
  * @property {Array.<import('./attendee').attendee>} users - array of users
  */
 /** @type {booking} */
 export const booking = deepFreeze({
-  type: Values.BOOKING_TYPES.UNDEFINED,
+  type: null,
   users: [],
 })
