6d309fdff7d24ad1645cebe2977a56dd9a42b044
Daniel Yo <dnl992004@yahoo.com.au>
2020-06-23 16:36:33 -0700

    typo on jsdoc

--- src/models/attendee.js
+++ src/models/attendee.js
@@ -7,7 +7,7 @@ import { deepFreeze } from 'jsutils'
  * @property {string} bookedTicketIdentifier - booked ticket id
  * @property {string} name - attendee's name
  * @property {string} attendeeCategoryIdentifier - attendee category id
- * @property {number|string} bookedDays - list of days booked
+ * @property {Array<number>} bookedDays - list of days booked
  * @property {Array<string>} bookedSessions - list of sessions booked
  */
 /** @type {attendee} */
