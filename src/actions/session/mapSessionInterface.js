037caedf08d2a87411eb26d71e50f57ce4691e1d
Daniel Yo <dnl992004@yahoo.com.au>
2020-06-25 14:02:13 -0700

    update: - removed unused file - updated mapSessionInterface to reduce redundancy

--- src/actions/session/mapSessionInterface.js
+++ src/actions/session/mapSessionInterface.js
@@ -2,68 +2,46 @@
 import { ActionTypes } from 'SVConstants'
 import { Values } from 'SVConstants'
 import { dispatch } from '../../store/sessionsStore'
+import { mapObj } from 'jsutils'
 
 const { CATEGORIES, SUB_CATEGORIES } = Values
+
+/**
+ * defines which sessionAgendaProps need to be mapped differently
+ */
+const subCatMap = {
+  settings: SUB_CATEGORIES.AGENDA_SETTINGS,
+}
 
 /**
  * push the sessionAgendaProps items to our local state
  * @param {import('SVModels/sessionAgendaProps').sessionAgendaProps} props
  */
 export const mapSessionInterface = props => {
-  if (!props) return
+  // loop through each key and dispatch accordingly
+  props &&
+    mapObj(props, (key, value) => {
+      // ensure key exists in the local storage first
+      if (key === CATEGORIES[key.toUpperCase()]) {
+        let type = ActionTypes.UPSERT_ITEMS
+        let payload = {
+          category: key,
+          items: value,
+        }
 
-  props.attendees &&
-    dispatch({
-      type: ActionTypes.UPSERT_ITEMS,
-      payload: {
-        category: CATEGORIES.ATTENDEES,
-        items: props.attendees,
-      },
-    })
-
-  props.presenters &&
-    dispatch({
-      type: ActionTypes.UPSERT_ITEMS,
-      payload: {
-        category: CATEGORIES.PRESENTERS,
-        items: props.presenters,
-      },
-    })
-
-  props.sessions &&
-    dispatch({
-      type: ActionTypes.UPSERT_ITEMS,
-      payload: {
-        category: CATEGORIES.SESSIONS,
-        items: props.sessions,
-      },
-    })
-
-  props.settings &&
-    dispatch({
-      type: ActionTypes.UPSERT_ITEM,
-      payload: {
-        category: CATEGORIES.SETTINGS,
-        key: SUB_CATEGORIES.AGENDA_SETTINGS,
-        item: props.settings,
-      },
-    })
-
-  props.locations &&
-    dispatch({
-      type: ActionTypes.UPSERT_ITEMS,
-      payload: {
-        category: CATEGORIES.LOCATIONS,
-        items: props.locations,
-      },
-    })
-
-  props.labels &&
-    dispatch({
-      type: ActionTypes.UPSERT_ITEMS,
-      payload: {
-        category: CATEGORIES.LABELS,
-        items: props.labels,
-      },
+        // certain props need to be mapped to a specific key
+        if (subCatMap[key]) {
+          type = ActionTypes.UPSERT_ITEM
+          payload = {
+            category: key,
+            item: value,
+            key: subCatMap[key],
+          }
+        }
+        dispatch({
+          type,
+          payload,
+        })
+      }
     })
 }
