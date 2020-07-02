edebda4c481e3db8cca86da8f10718661f9dea53
Daniel Yo <dnl992004@yahoo.com.au>
2020-07-02 16:28:39 -0700

    update: - added modal actions - renamed sessionsComponent to sessions

--- src/components/sessions.js
+++ src/components/sessions.js
@@ -7,6 +7,26 @@ import { sortLabels } from 'SVUtils'
 import testData from '../mocks/eventsforce/testData'
 
 /**
+ * TODO: Move to utils
+ * @param {Array} types
+ */
+const renderModals = types => {
+  console.log(types)
+  return types.map(type => {
+    switch (type) {
+    case Values.MODAL_TYPES.ERROR:
+      return ModalError
+    case Values.MODAL_TYPES.PRESENTERS:
+      return null
+    case Values.MODAL_TYPES.FILTER:
+      return ModalFilter
+    default:
+      return null
+    }
+  })
+}
+
+/**
  * SessionComponent
  * @param {import('SVModels/sessionAgendaProps').SessionAgendaProps} props - session agenda props defined in evf interface
  */
@@ -27,6 +47,32 @@ export const Sessions = props => {
       <Text>Active session id: { store.activeSession.id }</Text>
       <Text>Sessions count: { store.sessions.length }</Text>
       <Text>Attendees count: { store.attendees.length }</Text>
+      <Button
+        themePath='button.contained.primary'
+        onClick={() => addModal(Values.MODAL_TYPES.ERROR, cb)}
+        content={'toggle modal error'}
+      />
+      <Button
+        themePath='button.contained.primary'
+        onClick={() => addModal(Values.MODAL_TYPES.FILTER, cb)}
+        content={'toggle modal filter'}
+      />
+      <Button
+        themePath='button.contained.primary'
+        onClick={() => {
+          removeModal(store.modals)
+        }}
+        content={'remove modal no index'}
+      />
+
+      <Button
+        themePath='button.contained.primary'
+        onClick={() => {
+          removeModal(store.modals, 5)
+        }}
+        content={'remove modal with index'}
+      />
+      { renderModals(store.modals) }
     </View>
   )
 }
