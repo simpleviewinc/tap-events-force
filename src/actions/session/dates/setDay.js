80fdcdf708e887661d05caf6da881979d4f0306b
mpcarolin <mpcarolin.dev@gmail.com>
2020-08-05 09:10:44 -0700

    updated to use latest jsutils

--- src/actions/session/dates/setDay.js
+++ src/actions/session/dates/setDay.js
@@ -1,21 +1,14 @@
 import { ActionTypes, Values } from 'SVConstants'
 import { dispatch } from 'SVStore'
-import { validate, isNum } from 'jsutils'
+import { validate, isPositive } from 'jsutils'
 const { CATEGORIES } = Values
-
-/**
- * isPositiveNumber
- * @param {Number} num
- * @return {boolean} - true if the day is a positive number
- */
-const isPositiveNumber = num => isNum(num) && num > 0
 
 /**
  * Sets the current, actively-selected day in the agenda
  * @param {Number} newDayNumber - number to change the day to
  */
 export const setDay = newDayNumber => {
-  const [valid] = validate({ newDayNumber }, { newDayNumber: isPositiveNumber })
+  const [valid] = validate({ newDayNumber }, { newDayNumber: isPositive })
   if (!valid) return
 
   dispatch({
--- src/utils/models/agenda/agendaDays/getLatestDay.js
+++ src/utils/models/agenda/agendaDays/getLatestDay.js
@@ -1,16 +1,4 @@
-/**
- * Returns the maximum element as identified by the prop extracted from propSelector
- * @param {Array<Object>} arr
- * @param {Function} propSelector
- * @example
- * const items = [ { num: 1 }, { num: 3 } ]
- * findMax(items, item => item.num) // returns { num: 3 }
- */
-const findMax = (arr = [], propSelector) =>
-  arr.length &&
-  arr.reduce((maxSoFar, next) => {
-    return propSelector(next) > propSelector(maxSoFar) ? next : maxSoFar
-  })
+import { findMax } from 'jsutils'
 
 /**
  * Returns the latest day in the list of agenda days
