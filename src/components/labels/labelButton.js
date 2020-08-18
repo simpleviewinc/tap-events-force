d4bb053510c940feb992bc316861b4ca6b1dc67e
Daniel Yo <dnl992004@yahoo.com.au>
2020-08-18 13:52:06 -0700

    update: - cleaning up unused styles - moved some styles over for clarity - updated labelButton styling so it has the active styling

--- src/components/labels/labelButton.js
+++ src/components/labels/labelButton.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { Button } from 'SVComponents'
 import { useTheme } from '@simpleviewinc/re-theme'
 import { get } from 'jsutils'
+
 /**
  * Returns the label styles based on the given label className
  * this is so we can customize the hover and active button if needed
@@ -36,10 +37,11 @@ export const LabelButton = ({ style = {}, label = {}, onPress }) => {
     // we stringify theme.eventsForce because the theme object changes, but we only care about theme.eventsForce prop
     useMemo(() => getEvfLabelStyle(theme, label), [
       JSON.stringify(get(theme, `eventsForce.${label.className}`)),
-      label,
+      label.className,
     ]),
     { style }
   )
+
   const clickHandler = () => onPress && onPress(label)
   return (
     <Button
