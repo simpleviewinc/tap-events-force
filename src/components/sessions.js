ef82703c3e479f39f2986146353bfddd6b31d096
Daniel Yo <dnl992004@yahoo.com.au>
2020-08-28 12:13:02 -0700

    update: - put the agendaSessions in useMemo. from feedback: https://github.com/simpleviewinc/tap-events-force/pull/40#discussion_r478851122

--- src/components/sessions.js
+++ src/components/sessions.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useCallback } from 'react'
-import { useTheme } from '@svkeg/re-theme'
+import React, { useEffect, useCallback, useMemo } from 'react'
+import { useTheme, useDimensions } from '@svkeg/re-theme'
+import { View, AppHeader, Button } from 'SVComponents'
+import { RenderModals } from 'SVComponents/modal/renderModals'
 import { mapSessionInterface } from 'SVActions/session/mapSessionInterface'
 import { incrementDay, decrementDay } from 'SVActions/session/dates'
-import { RenderModals } from 'SVComponents/modal/renderModals'
 import { GridContainer } from 'SVContainers/gridContainer'
 import { useSelector, shallowEqual } from 'react-redux'
 import { useAgenda } from 'SVHooks/models/useAgenda'
@@ -17,8 +18,9 @@ import { pickKeys, mapObj, get } from '@svkeg/jsutils'
  * @param {object} props
  * @param {object} props.styles
  * @param {Function} props.onClick
+ * @param {object} props.dataSet
  */
-const FilterButton = ({ onClick, styles }) => {
+const FilterButton = ({ onClick, styles, dataSet }) => {
   const dim = useDimensions()
 
   const contentStyles = styles?.content
@@ -27,14 +29,14 @@ const FilterButton = ({ onClick, styles }) => {
   return dim.width <= 650 ? (
     <EVFIcons.Filter
       style={contentStyles?.filterIcon}
-      dataSet={Sessions.dataSet.content.header.content.right.filterIcon}
+      dataSet={dataSet?.content?.filterIcon}
       onPress={onClick}
       color={contentStyles?.filterIcon?.color}
     />
   ) : (
     <Button
       themePath='button.text.default'
-      dataSet={Sessions.dataSet.content.header.content.right.filterButton}
+      dataSet={dataSet?.content?.filterButton}
       styles={contentStyles?.filterButton}
       onClick={onClick}
       content={'Filter'}
@@ -61,7 +63,7 @@ const SessionsHeader = ({ styles, onDayChange }) => {
 
   return (
     <AppHeader
-      dataSet={Sessions.dataSet.content.header.main}
+      dataSet={Sessions.dataSet.content.header}
       styles={headerStyles}
       CenterComponent={
         <DayToggle
@@ -82,6 +84,30 @@ const SessionsHeader = ({ styles, onDayChange }) => {
       }
     />
   )
+}
+
+/**
+ * Sets up the container for a group of sessions on a specific day
+ * @param {object} props
+ * @param {object} props.labels
+ * @param {object} props.daySessions - group of sessions in the form of {'9:15': [sessionA, sessionB,..]}
+ * @returns {Component}
+ */
+const AgendaSessions = ({ labels, daySessions }) => {
+  return useMemo(() => {
+    if (!daySessions) return null
+    return mapObj(daySessions, (timeBlock, sessions) => {
+      return (
+        // creates a gridContainer separated by hour blocks
+        <GridContainer
+          key={timeBlock}
+          sessions={sessions}
+          labels={labels}
+          timeBlock={timeBlock}
+        />
+      )
+    })
+  }, [ daySessions, labels ])
 }
 
 /**
@@ -113,22 +139,12 @@ export const Sessions = props => {
         styles={sessionsStyles}
         onDayChange={onDayChange}
       />
-      {
-        // creates a gridContainer separated by hour blocks
-        mapObj(
-          agendaSessions[settings?.agendaSettings?.activeDayNumber || 1],
-          (key, sessions) => {
-            return (
-              <GridContainer
-                key={key}
-                sessions={sessions}
-                labels={labels}
-                timeBlock={key}
-              />
-            )
-          }
-        )
-      }
+      <AgendaSessions
+        labels={labels}
+        daySessions={
+          agendaSessions[settings?.agendaSettings?.activeDayNumber || 1]
+        }
+      />
       { modals.length > 0 && RenderModals(modals) }
     </View>
   )
@@ -141,11 +157,13 @@ Sessions.dataSet = {
       main: { class: 'sessions-content-header-main' },
       content: {
         right: {
-          filterIcon: {
-            class: 'sessions-content-header-content-right-filter-icon',
-          },
-          filterButton: {
-            class: 'sessions-content-header-content-right-filter-button',
+          content: {
+            filterIcon: {
+              class: 'sessions-content-header-content-right-filter-icon',
+            },
+            filterButton: {
+              class: 'sessions-content-header-content-right-filter-button',
+            },
           },
         },
       },
