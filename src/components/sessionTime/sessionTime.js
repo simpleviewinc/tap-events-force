418f1e984bbd9425e5df14c1e0660054c105b75f
lancetipton <lancetipton04@gmail.com>
2020-08-28 03:16:04 -0700

    Updates * Minor updates to make working on the useCss hook esier * Most of this will be reverted

--- src/components/sessionTime/sessionTime.js
+++ src/components/sessionTime/sessionTime.js
@@ -3,6 +3,7 @@ import { useTheme } from '@keg-hub/re-theme'
 import PropTypes from 'prop-types'
 import { View, Text } from '@keg-hub/keg-components'
 import { getTimeFromDate } from 'SVUtils/dateTime'
+import { EVFIcons } from 'SVIcons'
 
 /**
  * SessionTime
@@ -19,21 +20,11 @@ export const SessionTime = props => {
   const clockStyle = theme.get('sessionTime.clockIcon')
   const textStyle = theme.get('sessionTime.timeText')
 
-  // TODO: This should be moved to ReTheme
-  // It should use a lookup table instead of all these get calls
-  const dataRefs = get(theme, `eventsForce.dataClasses.objRef`)
-  const dataClassRef = dataRefs[SessionTime.dataSet.timeText.class]
-  const dataClassStyles = get(
-    theme,
-    `eventsForce.dataClasses.asObj.${dataClassRef}`
-  )
-  const cssProps = useCss(textStyle.content, dataClassStyles)
-
   return (
     <View style={mainStyle}>
-      <View style={textStyle.main}>
       <EVFIcons.Clock style={clockStyle.main} />
-        <Text {...cssProps}>
+      <View style={textStyle.main}>
+        <Text style={textStyle.content}>
           { `${getTimeFromDate(start, military)} - ${getTimeFromDate(
             end,
             military
@@ -49,10 +40,4 @@ SessionTime.propTypes = {
   start: PropTypes.string,
   end: PropTypes.string,
   military: PropTypes.bool,
-}
-
-SessionTime.dataSet = {
-  main: { class: 'session-time-main' },
-  clockIcon: { class: 'ef-sessions-date-time' },
-  timeText: { class: 'ef-sessions-date-time' },
 }
