cfdf906da47987d0cda7b532e1c178c19b6f7540
Daniel Yo <dnl992004@yahoo.com.au>
2020-09-02 12:50:27 -0700

    updated: - updated attendee model with the waitingList array

--- src/models/attendee.js
+++ src/models/attendee.js
@@ -6,6 +6,7 @@ export class Attendee {
   attendeeCategoryIdentifier = ''
   bookedDays = []
   bookedSessions = []
+  waitingListSessions = []
 
   /**
    * Attendee class model
@@ -15,6 +16,7 @@ export class Attendee {
    * @param {string=} params.attendeeCategoryIdentifier - attendee category id
    * @param {Array<number>=} params.bookedDays - list of days booked
    * @param {Array<string>=} params.bookedSessions - list of sessions booked
+   * @param {Array<string>=} params.waitingListSessions - list of sessions on waiting list
    */
   constructor(params = {}) {
     assignDefinedProps(this, params)
