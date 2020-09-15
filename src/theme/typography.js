26bd4a04d26d2c02f96bd8debc40fc2b1ad142be
Lance Tipton <lancetipton04@gmail.com>
2020-09-15 15:39:03 -0700

    Updates   * Set Inter as the default font

--- src/theme/typography.js
+++ src/theme/typography.js
@@ -6,8 +6,8 @@ import { colors } from './colors'
  * @object
  */
 const fonts = {
-  headings: 'system',
-  default: 'system',
+  headings: 'Inter, Verdana, Geneva, sans-serif',
+  default: 'Inter, Verdana, Geneva, sans-serif',
 }
 
 /**
