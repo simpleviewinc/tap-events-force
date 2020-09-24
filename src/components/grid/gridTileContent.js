ad3b538b868583db3242c6d1b224e680a843c3c7
Daniel Yo <dnl992004@yahoo.com.au>
2020-09-24 13:20:00 -0700

    update: - added evfButton to gridTileContent - styled accordingly

--- src/components/grid/gridTileContent.js
+++ src/components/grid/gridTileContent.js
@@ -7,6 +7,7 @@ import { SessionTime } from 'SVComponents/sessionTime/sessionTime'
 import PropTypes from 'prop-types'
 import { useTheme } from '@keg-hub/re-theme'
 import { useFormattedPrice } from 'SVHooks/models/price'
+import { EvfButton } from 'SVComponents/button/evfButton'
 
 /**
  * The content of a grid item when displayed as a tile (> 480px width)
@@ -64,6 +65,15 @@ export const GridTileContent = props => {
         onPress={() => console.log('Open session details modal')}
         text={session?.name}
       />
+
+      <View style={gridTileContentStyles?.buttonSection?.main}>
+        <EvfButton
+          type={'primary'}
+          styles={gridTileContentStyles?.buttonSection?.bookingButton}
+          // onClick={onButtonPress}
+          text={'SELECT'}
+        />
+      </View>
     </View>
   )
 }
--- src/theme/components/grid/gridTileContent.js
+++ src/theme/components/grid/gridTileContent.js
@@ -1,18 +1,37 @@
 export const gridTileContent = {
   main: {
+    flex: 1,
     width: '100%',
     flexDirection: 'column',
+    marginRight: 12,
   },
   row1: {
     main: {
-      flex: 1,
       flexDirection: 'row',
     },
     price: {
       fontWeight: 'bold',
       fontSize: 16,
       lineHeight: 19,
-      marginRight: 12,
+    },
+  },
+  buttonSection: {
+    main: {
+      $web: {
+        position: 'absolute',
+        bottom: 0,
+        alignSelf: 'flex-end',
+      },
+    },
+    bookingButton: {
+      main: {
+        height: 50,
+      },
+      content: {
+        button: {
+          main: {},
+        },
+      },
     },
   },
 }
