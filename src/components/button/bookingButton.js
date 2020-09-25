f2ce6632a8f4faf95eebdb840643b66c3f1fc6ae
Daniel Yo <dnl992004@yahoo.com.au>
2020-09-24 17:38:32 -0700

    added a check for isWaitingListAvailable for bookingBtn

--- src/components/button/bookingButton.js
+++ src/components/button/bookingButton.js
@@ -6,14 +6,23 @@ import { EvfButton } from 'SVComponents/button/evfButton'
  *       - https://jira.simpleviewtools.com/browse/ZEN-371
  *       - https://jira.simpleviewtools.com/browse/ZEN-268
  * @param {object} props
+ * @param {object} props.styles
+ * @param {import('SVModels/session').Session} props.session
  */
 export const BookingButton = ({ styles, session }) => {
   if (!session) return null
 
   // placeholder logic for 'disabled' btn
   // this should be removed once button states logic / ui are implemented
-  if (session.capacity?.remainingPlaces <= 0) return null
+  if (
+    session.capacity?.remainingPlaces <= 0 &&
+    !session.capacity?.isWaitingListAvailable
+  )
+    return null
 
+  // we want to show the SELECT btn if:
+  // - waiting list is available
+  // - remainingPlaces > 0
   return (
     <EvfButton
       type={'primary'}
