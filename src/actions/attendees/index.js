a6da2fafb0072af5d71d1a2105016d023409f0ea
mpcarolin <mpcarolin.dev@gmail.com>
2020-09-28 13:15:55 -0700

    refactored new util file and added comments

--- src/actions/attendees/index.js
+++ src/actions/attendees/index.js
@@ -1,3 +1,4 @@
 export * from './setAttendeesByTicket'
 export * from './setRestrictedAttendeeIds'
 export * from './initSortedAttendees'
+export * from './initRestrictedAttendees'
---
+++ src/actions/attendees/initRestrictedAttendees.js
@@ -0,0 +1,18 @@
+import { buildRestrictedAttendeeSet } from 'SVUtils/models/attendees'
+import { setRestrictedAttendeeIds } from './setRestrictedAttendeeIds'
+
+/**
+ * Builds and sets in the items store the restricted attendees array for each session
+ * @param {Array<Session>} sessions
+ * @param {Array<Attendee>} attendees
+ * @return {void}
+ */
+export const initRestrictedAttendees = (sessions, attendees) => {
+  sessions.map(session => {
+    const restrictedAttendeeIds = buildRestrictedAttendeeSet(attendees, session)
+    setRestrictedAttendeeIds(
+      session.identifier,
+      Array.from(restrictedAttendeeIds)
+    )
+  })
+}
