f000e2b7c8716069b8b574ee480d88ceccb25582
mpcarolin <mpcarolin.dev@gmail.com>
2020-09-29 14:51:32 -0700

    removed debug logs

--- src/actions/session/booking/sessionBookingRequest.js
+++ src/actions/session/booking/sessionBookingRequest.js
@@ -8,12 +8,6 @@ const kegEventEmitter = getEventEmitter()
  * sessionBookingRequest
  */
 export const sessionBookingRequest = (sessionId, attendeeIds = []) => {
-  // TODO: incomplete until middle section is done
-  // https://jira.simpleviewtools.com/browse/ZEN-278
-  // 1. get selected attendee(s)
-  // 2. return 2 things
-  //     - session id (utilize activeSession state)
-  //     - the attendee.EfBookedTicketIdentifier in an array
   const valid = kegEventEmitter.emit(
     EVENTS.SESSION_BOOKING_REQUEST,
     sessionId,
