ae84f23f5aa05f344c1a835227d79130173ea2b5
Daniel Yo <dyo@simpleviewinc.com>
2020-09-30 14:27:37 -0700

    fixed date parsing for safari (#62)
    * fixed date parsing for safari
    
    * fix

--- src/utils/dateTime/getTimeFromDate.js
+++ src/utils/dateTime/getTimeFromDate.js
@@ -2,10 +2,12 @@ import { format } from 'date-fns'
 
 /**
  * Formats the given date to hh:mm format
- * @param {Date} date - some date ex: 2020-08-03 13:00:00. if null, it will take local date/time
+ * @param {string|Date} date - some date ex: 2020-08-03 13:00:00. if null, it will take local date/time
  * @param {boolean} military - whether to format in 24 hr or not
  * @returns {string}
  */
 export const getTimeFromDate = (date, military = true) => {
-  return format(new Date(date), military ? 'HH:mm' : 'h:mma')
+  let parsed = Date.parse(date)
+  if (isNaN(parsed)) parsed = new Date(date.replace(/-/g, '/'))
+  return format(parsed, military ? 'HH:mm' : 'h:mma')
 }
