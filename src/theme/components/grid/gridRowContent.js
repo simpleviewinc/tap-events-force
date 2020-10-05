9556b7c57ad2fd81245502db59c1991be8d55e2d
Daniel Yo <dnl992004@yahoo.com.au>
2020-10-05 14:46:26 -0700

    updating mobile view for session detail

--- src/theme/components/grid/gridRowContent.js
+++ src/theme/components/grid/gridRowContent.js
@@ -1,3 +1,5 @@
+import { colors } from '../../colors'
+
 export const gridRowContent = {
   main: {
     flex: 1,
@@ -10,6 +12,13 @@ export const gridRowContent = {
       flexDirection: 'column',
       pB: 10,
       pL: 10,
+    },
+    locationText: {
+      mT: 8,
+      ftSz: 16,
+      lnH: 19,
+      ftWt: '500',
+      color: colors.lightGray,
     },
   },
 }
