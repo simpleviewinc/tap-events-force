ddbe24dc2c4642928752b27b2928db2150f68970
Daniel Yo <dyo@simpleviewinc.com>
2020-10-15 09:41:30 -0700

    Zen 374 modal filter clear btn (#67)
    * update:
    - setup clear filters button
    
    * update so clear filter button only appears when we've selected some filters
    
    * caps
    
    * updated clear filter styles to not show bg color on active || hover
    
    * use no prop arr from jsutils
    
    * updated clear btn styles to be smaller and have hover styles back

--- src/theme/components/modals/filterModal.js
+++ src/theme/components/modals/filterModal.js
@@ -93,6 +93,22 @@ const buttonsWrapper = {
   },
 }
 
+const clearButtonDefault = {
+  main: {
+    jtC: 'flex-end',
+  },
+  content: {
+    $web: {
+      ltrS: 0.1,
+    },
+    $all: {
+      ftSz: 16,
+      ftWt: '500',
+      txDc: 'underline',
+      color: colors.lightGray01,
+    },
+  },
+}
 export const filterModal = {
   content: {
     main: {},
@@ -145,13 +161,18 @@ export const filterModal = {
       },
       bottomSection: {
         main: {
-          alignItems: 'flex-end',
+          flD: 'row',
+          alS: 'flex-end',
+          fl: 1,
+          flWr: 'wrap',
         },
-        button: {
+        applyButton: {
           main: {
             minHeight: 45,
+            mL: 15
           },
         },
+        clearButton: clearButtonDefault,
       },
     },
   },
