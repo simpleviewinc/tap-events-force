8a57915a8a4dd456b5d5380345982f177208554a
Daniel Yo <dnl992004@yahoo.com.au>
2020-10-15 11:22:21 -0700

    added chevron icons from the mobile mockup

---
+++ src/assets/icons/evf/elements/chevronDown.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { SvgIcon } from '@keg-hub/keg-components'
+
+export const ChevronDown = props => {
+  return (
+    <SvgIcon
+      fill='#020202'
+      width='18'
+      height='11'
+      viewBox='0 0 18 11'
+      delta='M.819.852a1.17 1.17 0 000 1.605l7.4 7.711a1.061 1.061 0 001.542 0l7.4-7.71A1.17 1.17 0 0017.2.851a1.062 1.062 0 00-1.542-.04l-.038.04-6.63 6.911L2.36.855A1.059 1.059 0 00.82.852c.002-.003 0 0 0 0z'
+      {...props}
+    />
+  )
+}
