2e784ebb190d5ab3574b898e734c967fbc3395e3
Daniel Yo <dnl992004@yahoo.com.au>
2020-10-26 14:17:56 -0700

    updated unit test

--- src/utils/filters/__tests__/sessionsFromStateFilters.js
+++ src/utils/filters/__tests__/sessionsFromStateFilters.js
@@ -1,18 +1,42 @@
-import { sessionsFromStateFilters } from '../sessionsFromStateFilters'
 import testData from '../../../mocks/eventsforce/testData'
 import { Label } from '../../../models/label'
+import { setState, getStore, dispatch } from '../../../mocks'
+import { Values } from 'SVConstants'
+
+jest.setMock('SVStore', { getStore, dispatch })
+const { sessionsFromStateFilters } = require('../sessionsFromStateFilters')
+const { SESSION_BOOKING_STATES } = Values
 
 describe('sessionsFromStateFilters', () => {
-  it('should return 5 results when filtering by `important` label', () => {
-    const labels = [testData.labels[0]] // 'important' label
-    const filteredSessions = sessionsFromStateFilters(labels, testData.sessions)
-    expect(filteredSessions.length).toEqual(5)
+  afterAll(() => jest.clearAllMocks())
+  beforeEach(() => {
+    dispatch.mockClear()
+    setState({
+      attendees: testData.attendees,
+      sessions: testData.sessions,
+    })
   })
 
-  it('should return no results when filtering by an unused label', () => {
-    const labels = [ 'z', 'y', 'x' ].map(str => new Label({ name: str }))
+  it('should return 2 results when filtering by `waitingList` && `onWaitingList` label', () => {
+    const labels = [
+      new Label({
+        identifier: 'WAITING_LIST',
+        name: SESSION_BOOKING_STATES.WAITING_LIST,
+      }),
+      new Label({
+        identifier: 'ON_WAITING_LIST',
+        name: SESSION_BOOKING_STATES.ON_WAITING_LIST,
+      }),
+    ]
 
     const filteredSessions = sessionsFromStateFilters(labels, testData.sessions)
-    expect(filteredSessions.length).toEqual(0)
+    expect(filteredSessions.length).toEqual(2)
+  })
+
+  it('should return original sessions list when filtering by an unused label', () => {
+    const labels = [ 'z', 'y', 'x' ].map(str => new Label({ identifier: str }))
+
+    const filteredSessions = sessionsFromStateFilters(labels, testData.sessions)
+    expect(filteredSessions.length).toEqual(testData.sessions.length)
   })
 })
