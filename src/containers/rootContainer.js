c3a740f221605af642ebba7a2f0e72b7a758e92a
mpcarolin <mpcarolin.dev@gmail.com>
2020-11-03 16:20:24 -0700

    started updates

--- src/containers/rootContainer.js
+++ src/containers/rootContainer.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
 import { withAppHeader } from 'SVComponents'
 import { SessionsContainer } from './sessionsContainer'
 import { displayName } from 'SVConfig'
-import testData from '../mocks/eventsforce/testData.js'
 import { isNative } from 'SVUtils/platform/isNative'
 import { TestData } from 'SVComponents/testData'
+import { getURLParam, get } from '@keg-hub/jsutils'
+import testData from '../mocks/eventsforce/testData.js'
+import * as bookingStatesTestData from '../mocks/eventsforce/bookingStates'
 
 const mockCallbacks = {
   onDayChange: day => console.log('Day changed to', day),
@@ -15,11 +17,24 @@ const mockCallbacks = {
 }
 
 /**
+ * Returns mock data for rootContainer, depending on if the state url
+ * parameter is set.
+ */
+const useTestDataState = () => {
+  const initialData = useMemo(() => {
+    const defaultPath = getURLParam('state') ?? ''
+    return get(bookingStatesTestData, defaultPath, testData)
+  }, [])
+
+  return useState(initialData)
+}
+
+/**
  * Root container for app Main.js
  * Currently only used in local development. Not exported by rollup (see apps/Sessions.js for that)
  */
 export const RootContainer = withAppHeader(displayName, props => {
-  const [ mockData, setMockData ] = useState(testData)
+  const [ mockData, setMockData ] = useTestDataState()
 
   return (
     <>
