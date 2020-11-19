492998d3ae835ecee84f20c61a659f9c1ee35d78
Lance Tipton <lancetipton04@gmail.com>
2020-11-19 14:10:49 -0700

    Type definition review - Optional (#97)
    [Semver-Status] patch
    
    * Updates
      * Added in defaults values for prop items that are optional
      * Made tickets && bookedTickets props optional
      * Added test booking state for optional props

--- src/mocks/eventsforce/bookingStates/index.js
+++ src/mocks/eventsforce/bookingStates/index.js
@@ -1,6 +1,7 @@
 export * from './conflict'
 export * from './fullyBooked'
 export * from './onWaitingList'
+export * from './optionalTickets'
 export * from './select'
 export * from './selected'
 export * from './waitingList'
