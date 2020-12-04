8d083870f19fb84ebb6cdb445e2561c2f12e22e5
mpcarolin <mpcarolin.dev@gmail.com>
2020-12-04 15:02:19 -0700

    fixed test

--- src/actions/modals/__tests__/removeModal.js
+++ src/actions/modals/__tests__/removeModal.js
@@ -5,41 +5,26 @@ jest.setMock('SVStore', { getStore, dispatch })
 const { removeModal } = require('SVActions/modals/removeModal')
 
 const modalData = {
-  modals: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }],
+  modals: {
+    activeModal: {},
+  },
 }
 
 describe('removeModal', () => {
   afterAll(() => jest.clearAllMocks())
   beforeEach(() => dispatch.mockClear())
 
-  it('should return warning if index is not within the array length', () => {
-    const spy = jest.spyOn(console, 'warn').mockImplementation()
-    removeModal(5)
-    expect(spy).toHaveBeenCalled()
-  })
-
-  it('should remove the last item if nothing is passed in', () => {
+  it('should remove the active modal', () => {
     const expectedPayload = {
       category: Values.CATEGORIES.MODALS,
-      items: [{ id: 1 }, { id: 2 }, { id: 3 }],
+      key: 'activeModal',
+      item: null,
     }
     setState(modalData)
     removeModal()
     const dispatchArgs = dispatch.mock.calls[0][0]
 
-    expect(dispatchArgs.type).toBe(ActionTypes.SET_ITEMS)
-    expect(dispatchArgs.payload).toMatchObject(expectedPayload)
-  })
-
-  it('should remove index 2 on modals array', () => {
-    const expectedPayload = {
-      category: Values.CATEGORIES.MODALS,
-      items: [{ id: 1 }, { id: 2 }, { id: 4 }],
-    }
-    setState(modalData)
-    removeModal(2)
-    const dispatchArgs = dispatch.mock.calls[0][0]
-    expect(dispatchArgs.type).toBe(ActionTypes.SET_ITEMS)
+    expect(dispatchArgs.type).toBe(ActionTypes.SET_ITEM)
     expect(dispatchArgs.payload).toMatchObject(expectedPayload)
   })
 })
