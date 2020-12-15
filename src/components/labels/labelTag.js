b7ecb34e2591577bdd3e84239f1673eb6a2b02b9
mpcarolin <mpcarolin.dev@gmail.com>
2020-12-15 15:48:28 -0700

    updated labelTag to have a default bg-color

--- src/components/labels/labelTag.js
+++ src/components/labels/labelTag.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import PropTypes from 'prop-types'
 import { View } from '@keg-hub/keg-components'
 import { useTheme } from '@keg-hub/re-theme'
+import { noPropObj } from '@keg-hub/jsutils'
+
+/**
+ * @param {Object} styles - custom styles
+ * @return {Object} styles for the LabelTag component
+ */
+const useLabelTagStyles = styles => {
+  const theme = useTheme()
+  const mainStyles = theme.get('labelTag.main')
+  const defaultSurfaces = theme.get('colors.surface.default.colors')
+
+  return useMemo(
+    () => ({
+      ...mainStyles,
+
+      // matches the default Button bg-color, in LabelButton on desktop web
+      backgroundColor: defaultSurfaces.dark,
+
+      ...styles,
+    }),
+    [ styles, defaultSurfaces ]
+  )
+}
 
 /**
  * LabelTag - a colored, square label without text
@@ -9,12 +32,13 @@ import { useTheme } from '@keg-hub/re-theme'
  * @param {import('SVModels/label').Label} props.label - the label model instance
  * @param {Object} props.styles - styles for the label. Overwrites styles defined in labelTag.main
  */
-export const LabelTag = ({ label = {}, styles }) => {
-  const theme = useTheme()
+export const LabelTag = ({ label = noPropObj, styles }) => {
+  const tagStyles = useLabelTagStyles(styles)
+
   return (
     <View
       className={[ 'ef-label-tag', label.className ]}
-      style={[ theme.get('labelTag.main'), styles ]}
+      style={tagStyles}
     />
   )
 }
