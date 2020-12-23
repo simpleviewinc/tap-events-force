87e15c3096f95d9dd8ecffeeed5babef6e025d4b
Daniel Yo <dyo@simpleviewinc.com>
2020-12-23 11:38:29 -0700

    Zen 458 group booking disable (#110)
    `semver`: patch
    * UPDATE:
    - added mockdata to test the edge case
    
    * update:
    - extracted getDisabled to its own file so we can unit test
    - updated to also disable if capacity is unlimited given the edge case
    - added unit test for getDisabled
    
    * remove console log
    
    * did more testing and minor updates
    
    * removed unused code

--- src/mocks/eventsforce/bookingStates/conflict.js
+++ src/mocks/eventsforce/bookingStates/conflict.js
@@ -1,59 +1,54 @@
 import testData from '../testData'
 
+const sessions = (isUnlimited = false) => [
+  {
+    allowBooking: true,
+    identifier: '1',
+    name: `state - TIME CONFLICT - ${isUnlimited ? 'UNLIMITED' : 'LIMITED'} A`,
+    dayNumber: 1,
+    startDateTimeLocal: '2020-08-03 09:00:00',
+    endDateTimeLocal: '2020-08-03 13:30:00',
+    presenterIdentifiers: [ '1', '2' ],
+    labelIdentifiers: [ '1', '2' ],
+    locationIdentifier: '1',
+    liveVideoUrl: 'https://us02web.zoom.us/j/1234',
+    recordedVideoUrl: 'https://www.youtube.com/watch?v=21X5lGlDOfg',
+    restrictToAttendeeCategories: [],
+    capacity: {
+      isUnlimited,
+      remainingPlaces: 5,
+    },
+    price: {
+      currency: 'USD',
+      amount: 923.0,
+    },
+  },
+  {
+    allowBooking: true,
+    identifier: '2',
+    name: `state - TIME CONFLICT - ${isUnlimited ? 'UNLIMITED' : 'LIMITED'} B`,
+    dayNumber: 1,
+    startDateTimeLocal: '2020-08-03 09:00:00',
+    endDateTimeLocal: '2020-08-03 13:30:00',
+    presenterIdentifiers: [ '1', '2' ],
+    labelIdentifiers: [ '1', '2' ],
+    locationIdentifier: '1',
+    liveVideoUrl: 'https://us02web.zoom.us/j/1234',
+    recordedVideoUrl: 'https://www.youtube.com/watch?v=21X5lGlDOfg',
+    restrictToAttendeeCategories: [],
+    capacity: {
+      isUnlimited,
+      remainingPlaces: 5,
+    },
+    price: {
+      currency: 'USD',
+      amount: 923.0,
+    },
+  },
+]
+
 const conflictTestData = {
   ...testData,
-  sessions: [
-    {
-      allowBooking: true,
-      // Custom identifier to flag that booking has been stopped for that session
-      // Will need to update when Events-Force gives us the real value
-      // bookingStopped: true,
-      identifier: '1',
-      name: 'state - TIME CONFLICT - LIMITED A',
-      dayNumber: 1,
-      startDateTimeLocal: '2020-08-03 09:00:00',
-      endDateTimeLocal: '2020-08-03 13:30:00',
-      presenterIdentifiers: [ '1', '2' ],
-      labelIdentifiers: [ '1', '2' ],
-      locationIdentifier: '1',
-      liveVideoUrl: 'https://us02web.zoom.us/j/1234',
-      recordedVideoUrl: 'https://www.youtube.com/watch?v=21X5lGlDOfg',
-      restrictToAttendeeCategories: [],
-      capacity: {
-        isUnlimited: false,
-        remainingPlaces: 5,
-      },
-      price: {
-        currency: 'USD',
-        amount: 923.0,
-      },
-    },
-    {
-      allowBooking: true,
-      // Custom identifier to flag that booking has been stopped for that session
-      // Will need to update when Events-Force gives us the real value
-      // bookingStopped: true,
-      identifier: '2',
-      name: 'state - TIME CONFLICT - LIMITED B',
-      dayNumber: 1,
-      startDateTimeLocal: '2020-08-03 09:00:00',
-      endDateTimeLocal: '2020-08-03 13:30:00',
-      presenterIdentifiers: [ '1', '2' ],
-      labelIdentifiers: [ '1', '2' ],
-      locationIdentifier: '1',
-      liveVideoUrl: 'https://us02web.zoom.us/j/1234',
-      recordedVideoUrl: 'https://www.youtube.com/watch?v=21X5lGlDOfg',
-      restrictToAttendeeCategories: [],
-      capacity: {
-        isUnlimited: false,
-        remainingPlaces: 5,
-      },
-      price: {
-        currency: 'USD',
-        amount: 923.0,
-      },
-    },
-  ],
   attendees: [
     {
       bookedTicketIdentifier: '1',
@@ -65,23 +60,44 @@ const conflictTestData = {
   ],
 }
 
+const extraAttendees = [
+  {
+    bookedTicketIdentifier: '2',
+    name: 'Mr James',
+    attendeeCategoryIdentifier: '1',
+    bookedDays: [1],
+    bookedSessions: [],
+  },
+  {
+    bookedTicketIdentifier: '3',
+    name: 'Mr Foo Bar',
+    attendeeCategoryIdentifier: '2',
+    bookedDays: [1],
+    bookedSessions: [],
+  },
+]
+
 export const conflict = {
   single: {
-    limited: conflictTestData,
+    limited: {
+      ...conflictTestData,
+      sessions: sessions(),
+    },
+    unlimited: {
+      ...conflictTestData,
+      sessions: sessions(true),
+    },
   },
   group: {
     limited: {
       ...conflictTestData,
-      attendees: [
-        ...conflictTestData.attendees,
-        {
-          bookedTicketIdentifier: '2',
-          name: 'Mr James',
-          attendeeCategoryIdentifier: '1',
-          bookedDays: [],
-          bookedSessions: [],
-        },
-      ],
+      attendees: [ ...conflictTestData.attendees, ...extraAttendees ],
+      sessions: sessions(),
+    },
+    unlimited: {
+      ...conflictTestData,
+      attendees: [ ...conflictTestData.attendees, ...extraAttendees ],
+      sessions: sessions(true),
     },
   },
 }
---
+++ src/utils/models/sessions/getDisabled.js
@@ -0,0 +1,49 @@
+import { Values } from 'SVConstants'
+import { isPositive, exists } from '@keg-hub/jsutils'
+const { SESSION_BOOKING_STATES } = Values
+
+/**
+ * Checks if the booking display should be disabled from interaction
+ * @param {Object} props
+ * @param {import('SVModels/session').Session} props.session
+ * @param {import('SVModels/PendingSession').PendingSession} props.pendingSession - currently pending session, if there is one
+ * @param {Number} props.bookableCount - Attendees that can book the current session
+ * @param {string} props.bookingMode - Current mode of booking for the session (single|group)
+ * @param {Object} props.timeConflicts - Key value pairs of attendees booked in conflicting sessions. { ${attendeeId}: ${sessionId} }
+ * @param {Object} state
+ *
+ * @returns {boolean} - If the display interaction should be disabled
+ */
+export const getDisabled = (
+  { session, pendingSession, bookableCount, bookingMode, timeConflicts },
+  state
+) => {
+  // if there is a different session awaiting the booking request result, all others are disabled
+  if (
+    pendingSession?.identifier &&
+    pendingSession.identifier !== session.identifier
+  )
+    return true
+  // If state is select, and in single booking mode and there's a time conflict
+  // Then the booking state should be disabled
+  if (
+    state === SESSION_BOOKING_STATES.SELECT &&
+    bookingMode === 'single' &&
+    timeConflicts
+  )
+    return true
+
+  const { capacity, allowBooking } = session
+
+  // no capacity if bookableCount === 0 OR waitingList with spaces is not available
+  const noCapacity =
+    (exists(bookableCount) && !isPositive(bookableCount)) ||
+    !(
+      capacity?.remainingPlaces ||
+      capacity?.isWaitingListAvailable ||
+      capacity?.isUnlimited
+    )
+
+  // disable if there's no space left or if everyone in the group is booked on a conflicting session
+  return !allowBooking || noCapacity ? true : false
+}
