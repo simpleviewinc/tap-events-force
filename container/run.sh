c9f3a650cf4d75ef763d0bef6c9db70d9acd859f
Lance Tipton <lancetipton04@gmail.com>
2021-02-05 17:26:09 -0700

    Image-From Updates (#118)
    [Semver-Status] minor
    
    * feat: Updated container files
      * Added changes needed for keg-cli 3.0.0
    
    * fix: Added KEG_COPY_LOCAL env back
    
    * fix(image-from): Fix docker-compose.yml path

--- container/run.sh
+++ container/run.sh
@@ -1,61 +1,5 @@
 #!/usr/bin/env
 
-TAP_PATH=/keg/tap
-
-if [[ "$DOC_APP_PATH" ]]; then
-  TAP_PATH="$DOC_APP_PATH"
-fi
-
-keg_message(){
-  echo $"[ KEG-CLI ] $1" >&2
-  return
-}
-
-# Runs yarn install at run time
-# Use when adding extra node_modules to keg-core without rebuilding
-keg_run_tap_yarn_setup(){
-
-  # Check if $KEG_NM_INSTALL exist, if it doesn't, then return
-  if [[ -z "$KEG_NM_INSTALL" ]]; then
-    return
-  fi
-
-  if [[ "$KEG_NM_INSTALL" != "core" ]]; then
-    # Navigate to the cached directory, and run the yarn install here
-    cd $TAP_PATH
-    keg_message "Running yarn setup for tap..."
-    yarn install
-  fi
-  
-  if [[ "$KEG_NM_INSTALL" ]]; then
-    if [[ -d "$TAP_PATH/node_modules/keg-core" ]]; then
-      keg_message "Running yarn install for keg-core..."
-      cd $TAP_PATH/node_modules/keg-core
-      yarn install
-    fi
-  fi
-
-}
-
-# Runs a Tap
-keg_run_the_tap(){
-
-  cd $TAP_PATH
-
-  if [[ -z "$KEG_EXEC_CMD" ]]; then
-    KEG_EXEC_CMD="tap:start"
-  fi
-
-  # This is a temp fix until we are able to update expo
-  if [[ -d "$TAP_PATH/node_modules/keg-core/.expo" ]]; then
-    rm -rf $TAP_PATH/node_modules/keg-core/.expo
-  fi
-
-  keg_message "Running command 'yarn $KEG_EXEC_CMD'"
-  yarn $KEG_EXEC_CMD
-
-}
-
 # If the no KEG_DOCKER_EXEC env is set, just sleep forever
 # This is to keep our container running forever
 if [[ -z "$KEG_DOCKER_EXEC" ]]; then
@@ -64,9 +8,25 @@ if [[ -z "$KEG_DOCKER_EXEC" ]]; then
 
 else
 
-  # Run yarn setup for any extra node_modules to be installed
-  keg_run_tap_yarn_setup
+  # Ensure the DOC_APP_PATH is set
+  if [[ -z "$DOC_APP_PATH" ]]; then
+    DOC_APP_PATH=/keg/tap
+  fi
 
-  # Start the keg core instance
-  keg_run_the_tap
+  # cd into the tap repo
+  cd $DOC_APP_PATH
+
+  if [[ -z "$KEG_EXEC_CMD" ]]; then
+    KEG_EXEC_CMD="tap:start"
+  fi
+
+  # This is a temp fix until we are able to update expo
+  if [[ -d "$DOC_APP_PATH/node_modules/keg-core/.expo" ]]; then
+    rm -rf $DOC_APP_PATH/node_modules/keg-core/.expo
+  fi
+
+  # Start the tap instance
+  echo $"[ KEG-CLI ] Running command 'yarn $KEG_EXEC_CMD'" >&2
+  yarn $KEG_EXEC_CMD
+
 fi
