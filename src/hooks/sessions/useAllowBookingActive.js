8dc84dabceabd1e5282379d46238b21afd079142
Subaya Codanda <subaya.codanda@simpleviewinc.com>
2021-08-02 16:16:30 -0700

    ZEN-563: Part 2 of changes to ensure filter button is not displayed when there are no active filters (i.e. custom labels or active search criteria)

--- src/hooks/sessions/useAllowBookingActive.js
+++ src/hooks/sessions/useAllowBookingActive.js
@@ -4,9 +4,9 @@ import { Values } from 'SVConstants/values'
 const { CATEGORIES, SUB_CATEGORIES } = Values
 
 /**
- * Pulls the state of the waiting list from the store and returns it
+ * Pulls the state of the allow booking from the store and returns it
  *
- * @returns {boolean} - True if the waiting list is active
+ * @returns {boolean} - True if the allow booking is active
  */
 export const useAllowBookingActive = () => {
   const { settings } = useStoreItems([CATEGORIES.SETTINGS])
