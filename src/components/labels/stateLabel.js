1ba78f14d74c40b8df476a5bfff93727492a9564
Michael Carolin <mpcarolin.dev@gmail.com>
2021-08-20 00:21:35 +0000

    Use booking check styled component

--- src/components/labels/stateLabel.js
+++ src/components/labels/stateLabel.js
@@ -1,9 +1,8 @@
 import React, { useMemo } from 'react'
 import { getBookingState } from 'SVUtils/models/sessions/getBookingState'
-import { View, Text} from '@keg-hub/keg-components'
+import { View, Text } from '@keg-hub/keg-components'
 import { Values } from 'SVConstants/values'
 import { EVFIcons } from 'SVIcons'
-import { useStyle } from '@keg-hub/re-theme'
 import { reStyle } from '@keg-hub/re-theme/reStyle'
 import { useStoreItems } from 'SVHooks/store/useStoreItems'
 
@@ -17,63 +16,59 @@ const { BookingCheck } = EVFIcons
  * @param {import('SVModels/session').Session} props.session - the session model object
  */
 const useSessionState = session => {
-    const attendees = useStoreItems('attendees')
-    return useMemo(() => getBookingState(session), [ session, attendees ])
-  }
+  const attendees = useStoreItems('attendees')
+  return useMemo(() => getBookingState(session), [ session, attendees ])
+}
 
 const StateLabelText = reStyle(Text)((theme, props) => ({
   fontSize: 12,
   fontWeight: 500,
-  color: props.disabledText
-    ? theme.colors.black
-    : theme.colors.primary,
-  fontWeight: props.boldText
-    ? 800
-    : 500
+  color: props.disabledText ? theme.colors.black : theme.colors.primary,
+  fontWeight: props.boldText ? 800 : 500,
 }))
 
-const StateLabelView = reStyle(View)(()=> ({
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginRight: 15
+const StateLabelView = reStyle(View)(() => ({
+  flexDirection: 'row',
+  alignItems: 'center',
+  marginRight: 15,
 }))
 
-// const StateLabelCheck = reStyle(BookingCheck)(()=> ({
-//    fill: colors.primary,
-//    maxHeight: 18,
-// }))
+const StateLabelCheck = reStyle(
+  BookingCheck,
+  'styles'
+)(theme => ({
+  fill: theme.colors.primary,
+  maxHeight: 18,
+}))
 
 /**
  * The state label containing state and bookingcheck icon if applicable
- * @param {Object} props 
+ * @param {Object} props
  * @param {import('SVModels/session').Session} props.session - the session model object
  */
 export const StateLabel = props => {
-    if (!props.session) return null
-  
-    const { session } = props
-    const sessionState = useSessionState(session)
-    const labelToDisplay = (sessionState === SESSION_BOOKING_STATES.SELECT || sessionState === SESSION_BOOKING_STATES.READ_ONLY) 
-      ? '' 
-      : sessionState
-    const stateLabelStyles = useStyle('stateLabel')
-    const selected = sessionState === SESSION_BOOKING_STATES.SELECTED
-    const fullyBooked = sessionState === SESSION_BOOKING_STATES.FULLY_BOOKED
+  if (!props.session) return null
 
-    return (
-      <StateLabelView>
-        {/* TODO : Add state label class when received from trans-atlantic team */}
-        <StateLabelText boldText={selected} disabledText={fullyBooked}>
-          { labelToDisplay.toUpperCase() }
-        </StateLabelText>
-        { selected && 
-            <BookingCheck 
-              className='ef-button-text-icon' 
-              style={stateLabelStyles.check} 
-            /> 
-            // TODO: Use StateLabelCheck component from retheme
-            // <StateLabelCheck/>
-        }
-      </StateLabelView>
-    )
-  }
+  const { session } = props
+  const sessionState = useSessionState(session)
+  const labelToDisplay =
+    sessionState === SESSION_BOOKING_STATES.SELECT ||
+    sessionState === SESSION_BOOKING_STATES.READ_ONLY
+      ? ''
+      : sessionState
+  const selected = sessionState === SESSION_BOOKING_STATES.SELECTED
+  const fullyBooked = sessionState === SESSION_BOOKING_STATES.FULLY_BOOKED
+
+  return (
+    <StateLabelView>
+      { /* TODO : Add state label class when received from trans-atlantic team */ }
+      <StateLabelText
+        boldText={selected}
+        disabledText={fullyBooked}
+      >
+        { labelToDisplay.toUpperCase() }
+      </StateLabelText>
+      { selected && <StateLabelCheck /> }
+    </StateLabelView>
+  )
+}
