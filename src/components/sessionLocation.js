8d301a5df2ee68cb147dfc867a3bdc41e0cb6065
mpcarolin <mpcarolin.dev@gmail.com>
2021-09-10 17:39:59 -0700

    Add docs

--- src/components/sessionLocation.js
+++ src/components/sessionLocation.js
@@ -5,34 +5,42 @@ import { reStyle } from '@keg-hub/re-theme/reStyle'
 import { EVFIcons } from 'SVIcons'
 
 const LocationIcon = reStyle(EVFIcons.MapMarker)(
-  (_, props) => ({ mR: props.gap || 12 }), 
+  (_, props) => ({ mR: props.gap || 12 }),
   theme => ({ fill: theme.colors.iconGray })
 )
 
 const LocationRow = reStyle(View)({
   flD: 'row',
-  alI: 'center'
+  alI: 'center',
 })
 
 /**
  * Location icon and text
- * @returns 
+ * @param {Session} session
+ * @param {string} textClass - class for location text
+ * @param {number?} iconGap - optional gap between icon and text
+ * @param {Object} textStyle - style object for text
+ * @param {...*} - remaining props passesd to root view
  */
-export const SessionLocation = ({ session, textClass, iconGap, textStyle, ...viewProps }) => {
+export const SessionLocation = ({
+  session,
+  textClass,
+  iconGap,
+  textStyle,
+  ...viewProps
+}) => {
   const locationData = useSessionLocation(session)
   const locationName = locationData?.name
 
-  return locationName
-    ? (
-      <LocationRow {...viewProps}>
-        <LocationIcon gap={iconGap} />
-        <Text
-          className={textClass}
-          style={textStyle}
-        >
-          { locationName }
-        </Text>
-      </LocationRow>
-    )
-    : null
+  return locationName ? (
+    <LocationRow {...viewProps}>
+      <LocationIcon gap={iconGap} />
+      <Text
+        className={textClass}
+        style={textStyle}
+      >
+        { locationName }
+      </Text>
+    </LocationRow>
+  ) : null
 }
