1a9600da24b4ac9a556b9040b15c5cf0617335f3
Michael Carolin <mpcarolin.dev@gmail.com>
2021-09-14 15:52:31 -0700

    ZEN-627: Session Details Modal Updates (#139)
    * Update header styles and move labels down
    
    * Update styles
    - refactor with reStyle
    - remove unused code
    
    * Move category to EVF_CATEGORIES to fix bug
    
    * Refactor and clean up
    
    * Update keg-core version
    
    * Update w/ style aliases
    
    * Add exact border color from mockup
    
    * Remove horiz. padding in session details modal
    
    * Update reStyle call to use deepMerge
    
    * Fix style issues from QA
    - session details title now wraps
    - modal now has a minHeight of 250
    
    * Update docs

---
+++ src/components/modals/session/sessionDetailsModal.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { BaseModal } from '../baseModal'
+import { hideActiveModal } from 'SVActions/modals/hideActiveModal'
+import { SessionDetails } from './sessionDetails'
+import { reStyle } from '@keg-hub/re-theme/reStyle'
+
+const ModalBody = reStyle(SessionDetails)({ minH: 250, h: '100%' })
+
+/**
+ * SessionDetailsModal
+ * @param {object} props
+ * @param {import('SVModels/session').Session} props.session
+ * @param {boolean} props.visible
+ * @param {Array.<import('SVModels/label').Label>} props.labels - labels for this session
+ */
+export const SessionDetailsModal = ({ session, visible, labels }) => {
+  if (!session) return null
+
+  return (
+    <BaseModal
+      className='ef-modal-group'
+      hasCloseButton={true}
+      visible={visible}
+      Body={
+        <ModalBody
+          dismissModalCb={hideActiveModal}
+          session={session}
+          labels={labels}
+        />
+      }
+    />
+  )
+}
--- src/theme/components/modals/index.js
+++ src/theme/components/modals/index.js
@@ -2,12 +2,10 @@ import { alertModal } from './alertModal'
 import { filterModal } from './filterModal'
 import { groupBookingModal } from './groupBookingModal'
 import { presenterDetailsModal } from './presenterDetailsModal'
-import { sessionDetailsModal } from './sessionDetailsModal'
 
 export const modal = {
   groupBooking: groupBookingModal,
   presenter: presenterDetailsModal,
   alert: alertModal,
   filter: filterModal,
-  sessionDetails: sessionDetailsModal,
 }
