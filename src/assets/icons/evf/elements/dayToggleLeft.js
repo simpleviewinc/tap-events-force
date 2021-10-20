4d5daae7edb7e79d3b4eb98e1317b717c119291e
mpcarolin <mpcarolin.dev@gmail.com>
2021-10-20 08:08:36 -0700

    Icon Fixes

--- src/assets/icons/evf/elements/dayToggleLeft.js
+++ src/assets/icons/evf/elements/dayToggleLeft.js
@@ -1,22 +1,14 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import { SvgIcon } from '@keg-hub/keg-components'
 
 export function DayToggleLeft(props) {
-  const { style } = props
-
-  const dimStyles = useMemo(() => {
-    return {
-      height: style?.height || style?.fontSize || 17,
-      width: style?.width || style?.fontSize || 10,
-    }
-  }, [ style?.height, style?.width, style?.fontSize ])
-
   return (
     <SvgIcon
       viewBox='0 0 10 17'
       svgFill='none'
+      width={13}
+      height={23}
       {...props}
-      style={[ style, dimStyles ]}
       delta='M9.648.319a1.17 1.17 0 00-1.605 0l-7.711 7.4a1.061 1.061 0 000 1.542l7.71 7.4a1.17 1.17 0 001.606.039c.454-.414.47-1.103.04-1.542l-.04-.038-6.911-6.63 6.908-6.63A1.059 1.059 0 009.648.32c.003.002 0 0 0 0z'
       fill={props.fill || '#020202'}
     />
--- src/assets/icons/evf/elements/dayToggleRight.js
+++ src/assets/icons/evf/elements/dayToggleRight.js
@@ -1,22 +1,14 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import { SvgIcon } from '@keg-hub/keg-components'
 
 export function DayToggleRight(props) {
-  const { style } = props
-
-  const dimStyles = useMemo(() => {
-    return {
-      height: style?.height || style?.fontSize || 17,
-      width: style?.width || style?.fontSize || 10,
-    }
-  }, [ style?.height, style?.width, style?.fontSize ])
-
   return (
     <SvgIcon
       viewBox='0 0 10 17'
       svgFill='none'
+      width={13}
+      height={23}
       {...props}
-      style={[ style, dimStyles ]}
       delta='M.352 16.681a1.17 1.17 0 001.605 0l7.711-7.4a1.061 1.061 0 000-1.542l-7.71-7.4A1.17 1.17 0 00.351.3a1.062 1.062 0 000 1.58l6.911 6.63-6.908 6.63a1.059 1.059 0 00-.003 1.541c-.003-.002 0 0 0 0z'
       fill={props.fill || '#020202'}
     />
