3504982d03f72fd48a4fe09df438c251831f7561
mpcarolin <mpcarolin.dev@gmail.com>
2021-10-21 08:38:13 -0700

    Update presenter UI in session details modal

--- src/components/sessionDetails/sessionPresenters.js
+++ src/components/sessionDetails/sessionPresenters.js
@@ -1,19 +1,52 @@
 import React from 'react'
-import { getPresenterFullName, getPresenterProfession } from 'SVUtils/models'
+import { getPresenterFullName } from 'SVUtils/models'
 import { useSessionPresenters } from 'SVHooks/models'
 import { View, Text } from '@keg-hub/keg-components'
 import { reStyle } from '@keg-hub/re-theme/reStyle'
+import { EVFIcons } from 'SVIcons'
 
+/**
+ * Wraps presenter properties (icon and name)
+ */
+const ItemView = reStyle(View)((_, props) => ({
+  mT: props.isFirst ? 0 : 1,
+  flD: 'row',
+  alI: 'center',
+}))
+
+/**
+ * Icon for the presenter
+ */
+const PresenterIcon = reStyle(EVFIcons.User)({ mR: 10 }, theme => ({
+  fill: theme.colors.iconGray,
+  width: 14,
+  height: 16,
+  className: 'ef-session-presenter-icon',
+}))
+
+/**
+ * Name of the presenter
+ */
 const PresenterText = reStyle(Text)({
-  $xsmall: {
-    mT: 8,
-    ftSz: 14,
-  },
-  $small: {
-    mT: 20,
-    ftSz: 16,
-  }
+  $xsmall: { lnH: 24, ftSz: 14 },
+  $small: { ftSz: 16 },
 })
+
+/**
+ * @param {boolean} showIcon - show user icon if true
+ * @param {String} presenterName
+ * @param {String} textClassName - className for presenter text
+ * @param {*} ...props - remaining props passed to ItemView
+ */
+const PresenterItem = ({ presenter, textClassName, ...viewProps }) => {
+  const name = getPresenterFullName(presenter)
+  return (
+    <ItemView {...viewProps}>
+      <PresenterIcon />
+      <PresenterText className={textClassName}>{ name }</PresenterText>
+    </ItemView>
+  )
+}
 
 /**
  * Displays the full details of presenter(s) for the given session (name and profession)
@@ -22,25 +55,23 @@ const PresenterText = reStyle(Text)({
  * @param {object} props.styles
  * @param {object} props.textClassName
  */
-export const SessionPresenters = React.memo(({ session, textClassName }) => {
-  if (!session) return null
+export const SessionPresenters = React.memo(
+  ({ session, textClassName = 'ef-sessions-presenter', ...viewProps }) => {
+    if (!session) return null
 
-  const presenters = useSessionPresenters(session)
+    const presenters = useSessionPresenters(session)
 
-  return (
-    <View>
-      { presenters.map(presenter => {
-        const fullName = getPresenterFullName(presenter)
-        const profession = getPresenterProfession(presenter)
-        return (
-          <PresenterText
-            className={textClassName || 'ef-sessions-presenter'}
+    return (
+      <View {...viewProps}>
+        { presenters.map((presenter, idx) => (
+          <PresenterItem
+            isFirst={idx === 0}
+            presenter={presenter}
+            textClassName={textClassName}
             key={presenter.identifier}
-          >
-            { `${fullName}${profession && `, ${profession}`}` }
-          </PresenterText>
-        )
-      }) }
-    </View>
-  )
-})
+          />
+        )) }
+      </View>
+    )
+  }
+)
