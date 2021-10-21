6448806f41a9eac6dafca01aad12660d81542860
Michael Carolin <mpcarolin.dev@gmail.com>
2021-10-21 15:55:02 -0700

    Full app integration test (#153)
    * Add integration test for full Sessions component
    
    * Fix casing of import

--- src/testutils/testUtils.js
+++ src/testutils/testUtils.js
@@ -2,14 +2,33 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import { getDefaultTheme, ReThemeProvider } from '@keg-hub/re-theme'
 import { Provider } from 'react-redux'
+import { ComponentsProvider } from 'SVContexts/components/componentsProvider'
 import { getStore } from 'SVStore'
+import { evfModalBuilder } from '../mocks/eventsforce/evfModalBuilder'
+import { EvfButton } from '../mocks/eventsforce/evfButton'
+import 'SVTheme/tapIndex'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const TestRenderWrapper = ({ children }) => {
   const [activeTheme] = React.useState(getDefaultTheme())
 
+  // Replicating rootContainer.js setup
+  const SessionsModal = React.useMemo(
+    () => evfModalBuilder({ className: 'evf-modal' }),
+    []
+  )
+
   return (
     <Provider store={getStore()}>
-      <ReThemeProvider theme={activeTheme}>{ children }</ReThemeProvider>
+      <ReThemeProvider theme={activeTheme}>
+        <ComponentsProvider
+          ButtonComponent={EvfButton}
+          ModalComponent={SessionsModal}
+        >
+          { children }
+        </ComponentsProvider>
+      </ReThemeProvider>
     </Provider>
   )
 }
