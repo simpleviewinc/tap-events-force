6ee8cce161887a001a8b7c87e4f7560693b3c3cc
mpcarolin <mpcarolin.dev@gmail.com>
2022-02-21 16:18:01 -0700

    Improve booking submission tests

--- src/components/__tests__/groupBooking/bookingSubmission.integration.js
+++ src/components/__tests__/groupBooking/bookingSubmission.integration.js
@@ -2,13 +2,7 @@ import '@testing-library/jest-dom'
 import { waitFor } from 'testUtils'
 import testData from 'SVEvfMocks/eventsforce/testData.js'
 import { initModal, submitBooking, selectAttendeeCheckbox } from './testHelpers'
-import {
-  mixedListMock,
-  mockMixedSession,
-  teresa,
-  frank,
-  samantha,
-} from './mocks'
+import { mockMixedSession, teresa, penelope, frank, samantha } from './mocks'
 // import { prettyDOM } from 'testUtils'
 
 const mockOnlyBookingSession = {
@@ -21,36 +15,56 @@ const mockOnlyBookingSession = {
   },
 }
 
-const onlyBookingMock = {
-  ...testData,
-  sessions: [mockOnlyBookingSession],
-  attendees: [ samantha, teresa ],
+const mockOnlyWaitingSession = {
+  ...mockMixedSession,
+  name: 'Only booking',
+  capacity: {
+    isUnlimited: false,
+    remainingPlaces: 0,
+    isWaitingListAvailable: true,
+  },
 }
 
 describe('Group Booking Modal - Integration - Booking Submission', () => {
-  it('should fire the onSessionWaitinglistRequest callback upon waiting-list-only submission', async () => {
+  it('should fire the onSessionWaitingListRequest callback upon waiting-list-only submission', async () => {
     const {
       onSessionBookingRequest,
       onSessionWaitingListRequest,
-    } = await initModal(mixedListMock)
+    } = await initModal({
+      ...testData,
+      sessions: [mockOnlyWaitingSession],
+      attendees: [ samantha, penelope, frank ],
+    })
 
-    // frank is unselected at start
-    selectAttendeeCheckbox(frank.name)
+    // samantha is unselected at start
+    selectAttendeeCheckbox(samantha.name)
+
     submitBooking()
 
-    await waitFor(() =>
+    await waitFor(() => {
       expect(onSessionWaitingListRequest).toHaveBeenCalledTimes(1)
-    )
-    await waitFor(() =>
+      expect(onSessionWaitingListRequest).toHaveBeenCalledWith(
+        mockMixedSession.identifier,
+        expect.arrayContaining([
+          samantha.bookedTicketIdentifier,
+          penelope.bookedTicketIdentifier, // penelope is already on the wait list
+        ])
+      )
+    })
+    await waitFor(() => {
       expect(onSessionBookingRequest).toHaveBeenCalledTimes(0)
-    )
+    })
   })
 
   it('should fire the onSessionBookingRequest callback upon booking-list-only submission', async () => {
     const {
       onSessionBookingRequest,
       onSessionWaitingListRequest,
-    } = await initModal(onlyBookingMock)
+    } = await initModal({
+      ...testData,
+      sessions: [mockOnlyBookingSession],
+      attendees: [ samantha, teresa ],
+    })
 
     // teresa is unselected at start
     selectAttendeeCheckbox(teresa.name)
@@ -59,9 +73,14 @@ describe('Group Booking Modal - Integration - Booking Submission', () => {
     await waitFor(() =>
       expect(onSessionWaitingListRequest).toHaveBeenCalledTimes(0)
     )
-    await waitFor(() =>
+    await waitFor(() => {
       expect(onSessionBookingRequest).toHaveBeenCalledTimes(1)
-    )
+      expect(
+        onSessionBookingRequest
+      ).toHaveBeenCalledWith(mockOnlyBookingSession.identifier, [
+        teresa.bookedTicketIdentifier,
+      ])
+    })
   })
 
   it('should fire both callbacks when submitting both lists as modified', async () => {
@@ -78,11 +97,22 @@ describe('Group Booking Modal - Integration - Booking Submission', () => {
     selectAttendeeCheckbox(teresa.name)
     submitBooking()
 
-    await waitFor(() =>
-      expect(onSessionWaitingListRequest).toHaveBeenCalledTimes(1)
-    )
-    await waitFor(() =>
+    await waitFor(() => {
       expect(onSessionBookingRequest).toHaveBeenCalledTimes(1)
-    )
+      expect(
+        onSessionBookingRequest
+      ).toHaveBeenCalledWith(mockMixedSession.identifier, [
+        samantha.bookedTicketIdentifier,
+      ])
+    })
+
+    await waitFor(() => {
+      expect(onSessionWaitingListRequest).toHaveBeenCalledTimes(1)
+      expect(
+        onSessionWaitingListRequest
+      ).toHaveBeenCalledWith(mockMixedSession.identifier, [
+        teresa.bookedTicketIdentifier,
+      ])
+    })
   })
 })
