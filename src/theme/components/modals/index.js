2ab1a0a880205eacda2349f4188be0944e0bc776
Johnny Le <johnny.le@eventsforce.com>
2023-01-18 14:00:39 +0000

    Removed filter theme

--- src/theme/components/modals/index.js
+++ src/theme/components/modals/index.js
@@ -1,9 +1,7 @@
 import { alertModal } from './alertModal'
-import { filterModal } from './filterModal'
 import { groupBookingModal } from './groupBookingModal'
 
 export const modal = {
   groupBooking: groupBookingModal,
   alert: alertModal,
-  filter: filterModal,
 }
